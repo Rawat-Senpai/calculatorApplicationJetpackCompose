bf4124f6e0ce205d278fd9f1d17c5132f17fbbea
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-09-10 21:16:43 +0530

    code is pushed

--- app/src/main/java/com/example/calculatorjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/calculatorjetpackcompose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculatorjetpackcompose.ui.theme.CalculatorJetpackComposeTheme
 
@@ -22,7 +23,14 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<CalculatorViewModel>()
                 val state = viewModel.state
 
+                val buttonSpacing = 8.dp
 
+                Calculator(
+                    state = state ,
+                    modifier = Modifier ,
+                    buttonSpacing = buttonSpacing ,
+                    onAction = viewModel::onAction
+                )
 
             }
         }
